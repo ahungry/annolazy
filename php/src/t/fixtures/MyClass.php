4fdeae7773467ba8ed2f9bdef5f1598178bc4001
Matthew Carter <m@ahungry.com>
2017-07-12 23:48:14 -0400

    initial commit

---
+++ php/src/t/fixtures/MyClass.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Bla
+ *
+ * Long description.
+ *
+ * PHP version 7+
+ *
+ * @category Testing
+ * @package  Testing\MyClass
+ * @author   Matthew Carter <m@ahungry.com>
+ * @license  AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link     http://ahungry.com
+ */
+
+/**
+ * Bla
+ *
+ * @category Testing
+ * @package  Testing\MyClass
+ * @author   Matthew Carter <m@ahungry.com>
+ * @license  AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link     http://ahungry.com
+ */
+class MyClass
+{
+    /**
+     *
+     *
+     *
+     *
+     * @param
+     * @return
+     */
+    public function __construct(int $argOne, int $argTwo, string $argThree)
+    {
+        if (1 == 2) {
+            throw new \Exception();
+        }
+    }
+}

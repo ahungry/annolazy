19130d465e429461378062535d121d86643e2d27
Matthew Carter <m@ahungry.com>
2017-07-15 02:09:07 -0400

    Refactor short/long desc queries

--- php/src/Annolazy/Model/Doc.php
+++ php/src/Annolazy/Model/Doc.php
@@ -43,55 +43,18 @@ class Doc
         $this->lines = $lines;
     }
 
-    public function getIndexShortDesc()
+    public function getIndex(int $init = 0)
     {
         // The short desc is everything up until we hit the first empty line.
-        $start = 0;
-        $end   = 0;
+        $start  = $init;
+        $length = 0;
 
-        for ($i = 0; $i < count($this->lines); $i++) {
+        for ($i = $init; $i < count($this->lines); $i++) {
             $line = $this->lines[$i];
 
             // End position is found when we hit an empty line
             // after we have found a start position.
-            if (0 === strlen(trim($line)) && $start > 0) {
-                break;
-            }
-
-            if (0 < strlen(trim($line)) && $start === 0) {
-                $start = $i;
-            }
-
-            $end = $i;
-        }
-
-        return [$start, $end];
-    }
-
-    public function getShortDesc()
-    {
-        list($start, $end) = $this->getIndexShortDesc();
-
-        return trim(implode(' ', array_slice($this->lines, $start, 1+$end - $start)));
-    }
-
-    public function getIndexLongDesc()
-    {
-        // First, find the short (we can't have a long desc unless the short
-        // one exists).
-        list($start, $endS) = $this->getIndexShortDesc();
-
-        // The short desc is everything up until we hit the first empty line.
-        $start = 0;
-        $end   = 0;
-
-        for ($i = $endS + 1; $i < count($this->lines); $i++) {
-            $end = $i;
-            $line = $this->lines[$i];
-
-            // End position is found when we hit an empty line
-            // after we have found a start position.
-            if (0 === strlen(trim($line)) && $start > 0) {
+            if (0 === strlen(trim($line)) && $start > $init) {
                 break;
             }
 
@@ -100,20 +63,30 @@ class Doc
                 break;
             }
 
-            if (0 < strlen(trim($line)) && $start === 0) {
+            if (0 < strlen(trim($line)) && $start === $init) {
                 $start = $i;
             }
+
+            $length++;
         }
-        //var_dump ($start, $end); die;
 
+        return [$start, $length - 1];
+    }
 
-        return [$start, $end];
+    public function getShortDesc()
+    {
+        list($start, $end) = $this->getIndex();
+
+        return trim(implode(' ', array_slice($this->lines, $start, $end)));
     }
 
     public function getLongDesc()
     {
-        list($start, $end) = $this->getIndexLongDesc();
+        // First hit will be short, next will be long.
+        list($start, $end) = $this->getIndex();
 
-        return trim(implode(' ', array_slice($this->lines, $start, 1+$end - $start)));
+        list($start, $end) = $this->getIndex($start + $end);
+
+        return trim(implode(' ', array_slice($this->lines, $start, $end)));
     }
 }

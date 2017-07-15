f39358d5a37333ff9bf304e23af8f6f09caf18bb
Matthew Carter <m@ahungry.com>
2017-07-15 02:46:49 -0400

    Make binary file actually produce docblocks

--- php/src/bin/annolazy.php
+++ php/src/bin/annolazy.php
@@ -1,20 +1,28 @@
 <?php
 
-require __DIR__ . '/../t/fixtures/MyClass.php';
+if (empty($argv[1]) || !file_exists($argv[1])) {
+    echo 'Please pass a valid PHP file as the initial arg.';
+    exit(1);
+}
+
+//require __DIR__ . '/../t/fixtures/MyClass.php';
+require $argv[1];
 require __DIR__ . '/../Annolazy/Service/DocGenerator.php';
 require __DIR__ . '/../Annolazy/Service/Tokenizer.php';
 
 use Annolazy\Service\DocGenerator;
 use Annolazy\Service\Tokenizer;
-use Fake\Service\MyClass;
+//use Fake\Service\MyClass;
 
 $generator = new DocGenerator();
 //$generator->loadClass(MyClass::class);
 //echo $generator->getComment(MyClass::class, 'methods', 'foo');
 
 $tokenizer = new Tokenizer($generator);
-$tokenizer->loadFile(__DIR__ . '/../t/fixtures/MyClass.php');
-echo $tokenizer->parse();
+//$tokenizer->loadFile(__DIR__ . '/../t/fixtures/MyClass.php');
+$tokenizer->loadFile($argv[1]);
+$output = $tokenizer->parse();
+file_put_contents($argv[1], $output);
 
 //$tokenizer->loadFile(__DIR__ . '/../Annolazy/Service/DocGenerator.php');
 //echo $tokenizer->parse();

0320ce2d1f08aaa3911ed6ad421ff4b0670952e3
Matthew Carter <m@ahungry.com>
2017-07-15 00:06:00 -0400

    Add unit test and makefile things

---
+++ php/src/Annolazy/Model/Doc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Auto-generate documentation for a user.
+ *
+ * Who wants to waste time writing out what can be inferred?
+ *
+ * PHP version 7+
+ *
+ * @category Laziness
+ * @package  Annolazy
+ * @author   Matthew Carter <m@ahungry.com>
+ * @license  AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link     http://ahungry.com
+ */
+namespace Annolazy\Model;
+
+/**
+ * Auto-generate documentation for a user.
+ *
+ * Who wants to waste time writing out what can be inferred?
+ *
+ * PHP version 7+
+ *
+ * @category Laziness
+ * @package  Annolazy
+ * @author   Matthew Carter <m@ahungry.com>
+ * @license  AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link     http://ahungry.com
+ */
+class Doc
+{
+    private $comment;
+
+    public function __construct(string $comment)
+    {
+        $this->comment = $comment;
+    }
+}
---
+++ php/src/t/Model/DocTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Auto-generate documentation for a user.
+ *
+ * Who wants to waste time writing out what can be inferred?
+ *
+ * PHP version 7+
+ *
+ * @category Laziness
+ * @package  Annolazy
+ * @author   Matthew Carter <m@ahungry.com>
+ * @license  AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link     http://ahungry.com
+ */
+namespace Annolazy\t\Model;
+
+use Annolazy\Model\Doc;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Auto-generate documentation for a user.
+ *
+ * Who wants to waste time writing out what can be inferred?
+ *
+ * PHP version 7+
+ *
+ * @category Laziness
+ * @package  Annolazy
+ * @author   Matthew Carter <m@ahungry.com>
+ * @license  AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link     http://ahungry.com
+ */
+class DocTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $construct = new Doc();
+
+        $this->assertInstanceOf(
+            Doc::class,
+            $construct
+        );
+
+        return $construct;
+    }
+}
